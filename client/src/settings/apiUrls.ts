82ff132c0435a02b74f200984b2e607d0ff94d2a
Jontze <42588836+jontze@users.noreply.github.com>
2024-08-09 17:23:41 +0200

    feat(client): Dockerize react client

--- client/src/settings/apiUrls.ts
+++ client/src/settings/apiUrls.ts
@@ -1,13 +1,7 @@
-const GRAPHQL_ENDPOINT = import.meta.env.VITE_GRAPHQL_ENDPOINT as string;
-const GRAPHQL_WS_ENDPOINT = import.meta.env.VITE_GRAPHQL_WS_ENDPOINT as string;
-
-if (!GRAPHQL_ENDPOINT) {
-  throw Error('Expected environment variable: VITE_GRAPHQL_ENDPOINT');
-}
-
-if (!GRAPHQL_WS_ENDPOINT) {
-  throw Error('Expected environment variable: VITE_GRAPHQL_WS_ENDPOINT');
-}
+const GRAPHQL_ENDPOINT =
+  (import.meta.env.VITE_GRAPHQL_ENDPOINT as string) ?? '/graphql';
+const GRAPHQL_WS_ENDPOINT =
+  (import.meta.env.VITE_GRAPHQL_WS_ENDPOINT as string) ?? '/graphql';
 
 console.log('GRAPHQL_ENDPOINT', GRAPHQL_ENDPOINT);
 console.log('GRAPHQL_WS_ENDPOINT ', GRAPHQL_WS_ENDPOINT);
