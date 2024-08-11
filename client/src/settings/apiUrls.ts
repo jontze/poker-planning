860aefdd60de893393b3c81e490256cf22c7e5b8
Jontze <42588836+jontze@users.noreply.github.com>
2024-08-11 17:45:50 +0200

    chore(client): Allow to specify ws protocol and default to secure

--- client/src/settings/apiUrls.ts
+++ client/src/settings/apiUrls.ts
@@ -1,7 +1,12 @@
 const GRAPHQL_ENDPOINT =
   (import.meta.env.VITE_GRAPHQL_ENDPOINT as string) ?? '/graphql';
+const GRAPHQL_SECURE_WS_ENDPOINT =
+  (import.meta.env.VITE_GRAPHQL_SECURE_WS_ENDPOINT as string) === 'true';
 const GRAPHQL_WS_ENDPOINT =
-  (import.meta.env.VITE_GRAPHQL_WS_ENDPOINT as string) ?? '/graphql';
+  (import.meta.env.VITE_GRAPHQL_WS_ENDPOINT as string) ??
+  `${GRAPHQL_SECURE_WS_ENDPOINT ? 'wss://' : 'ws://'}${
+    window.location.host
+  }/graphql`;
 
 console.log('GRAPHQL_ENDPOINT', GRAPHQL_ENDPOINT);
 console.log('GRAPHQL_WS_ENDPOINT ', GRAPHQL_WS_ENDPOINT);
